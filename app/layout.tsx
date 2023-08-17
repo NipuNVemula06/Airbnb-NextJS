5eae5e7338079d1ba8122d76b007a19d5943ab81
BitcareUS <bitcaredemod@gmail.com>
2023-08-17 14:50:31 +0530

    Logout functionality and getting loged in user data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import ClientOnly from "./components/ClientOnly";
 import Modal from "./components/Modals/Modal";
 import RegisterModal from "./components/Modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/Modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -14,18 +16,21 @@ export const metadata: Metadata = {
   description: "Airbnb Clone by Nipun Vemula",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body suppressHydrationWarning={true} className={font.className}>
         <ClientOnly>
           <ToasterProvider />
           <RegisterModal />
-          <Navbar />
+          <LoginModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
